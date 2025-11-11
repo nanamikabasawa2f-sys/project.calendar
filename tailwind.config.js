b051558d3b02a784f0e049f5dd78c682dbdf74c7
ななみ <nanamin@nya-mi.local>
2025-11-11 19:07:43 +0900

    Tailwindの残りの設定ファイルを追加

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // srcフォルダ内の全てのJS/JSXファイル
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
