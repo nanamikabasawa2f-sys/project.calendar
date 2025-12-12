98c4a6cb097587275c6a7c7ed07f2315052fdc59
ななみ <nanamin@nya-mi.local>
2025-12-12 14:24:42 +0900

    firebase繋げたい

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // ★★★ ここにFirebaseコンソールで取得した設定情報を貼り付けてください ★★★
 const firebaseConfig = {
-  apiKey: "AIzaSyB-PK4-yYv34VrQM8ulnDOxtGghIoOW4Y8",
+  //apiKey: 
   authDomain: "project-2025-b92e7.firebaseapp.com",
   projectId: "project-2025-b92e7",
   storageBucket: "project-2025-b92e7.firebasestorage.app",
@@ -13,9 +13,7 @@ const firebaseConfig = {
   appId: "1:567550693268:web:b4a62977f57fe06cfd4981",
   measurementId: "G-Y5P08V9MEH"
 };
-// ★★★ 貼り付けはここまで ★★★
 
-// Firebaseを初期化
 const app = initializeApp(firebaseConfig);
 
 // Firestoreデータベースへの参照を取得
